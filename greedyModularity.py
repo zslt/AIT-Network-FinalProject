8d1cc71e0d904c562086e7b3d85726a0a13fc86e
KeczerZs <KeczerZs@KECZERZS-PC>
2018-05-05 15:47:32 +0200

    adding the main project file with reference to Newman (2004)

---
+++ greedyModularity.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 15:09:28 2018
+
+@author: KeczerZs
+
+
+This project is the implemenation of Newman's (2004) greedy modularity
+maximatization algorithm. The orgiginal paper can be accessed 
+at arXiv.org: https://arxiv.org/abs/cond-mat/0309508
+"""
+
